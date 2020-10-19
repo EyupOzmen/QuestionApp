35cbe2dd38339728bc834da1049402234a9f2590
EyupOzmen <ozmeneyup60@gmail.com>
2020-10-19 12:13:57 +0300

    "Radio Button"

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 import question from "../data/question.json";
 
 const Survey = () => {
+ 
+  const [selected,setSelected] = useState('')  
+
+  const handleChange = ( event ) => {
+      setSelected(event.target.value);
+  }
+
+  const handleSubmit = ( event ) => {
+      event.preventDefault();
+
+      alert(`Tuttuğunuz takım: ${selected}`);
+  }
+ 
   return (
     <div className="survey-container">
       <div className="survey-header">
@@ -10,22 +23,61 @@ const Survey = () => {
       <br/>
       <div className="survey-question">
       {console.log(question.Questions[0].Question)} 
-        <form>
-          
-          <p style={{color:"black"}} >{question.Questions[0].Question}</p>
-          <select
-            multiple={false}
-            //value={this.state.value}
-            //onChange={this.handleChange}
-          >
-            <option value={question.Questions[0].Options[0].ID}>{question.Questions[0].Options[0].Text}</option>
-            <option value={question.Questions[0].Options[1].ID}>{question.Questions[0].Options[1].Text}</option>
-            <option value={question.Questions[0].Options[2].ID}>{question.Questions[0].Options[2].Text}</option>
-            <option value={question.Questions[0].Options[3].ID}>{question.Questions[0].Options[3].Text}</option>
-          </select>
+      <form onSubmit={handleSubmit}>
+      <p>{question.Questions[0].Question}</p>
+      
+      <ul style={{listStyleType:"none"}} >
+        <li>
+          <label>
+            <input
+              type="radio"
+              value={question.Questions[0].Options[0].Text}
+              checked={selected === question.Questions[0].Options[0].Text}
+              onChange={handleChange}
+            />
+            {question.Questions[0].Options[0].Text}
+          </label>
+        </li>
+        
+        <li>
+          <label>
+            <input
+              type="radio"
+              value={question.Questions[0].Options[1].Text}
+              checked={selected === question.Questions[0].Options[1].Text}
+              onChange={handleChange}
+            />
+            {question.Questions[0].Options[1].Text}
+          </label>
+        </li>
 
-          <input type="submit" value="Submit" />
-        </form>
+        <li>
+          <label>
+            <input
+              type="radio"
+              value={question.Questions[0].Options[2].Text}
+              checked={selected === question.Questions[0].Options[2].Text }
+              onChange={handleChange}
+            />
+            {question.Questions[0].Options[2].Text}
+          </label>
+        </li>
+
+        <li>
+          <label>
+            <input
+              type="radio"
+              value={question.Questions[0].Options[3].Text}
+              checked={selected === question.Questions[0].Options[3].Text }
+              onChange={handleChange}
+            />
+            {question.Questions[0].Options[3].Text}
+          </label>
+        </li>
+      </ul>
+
+      <button type="submit">Cevapla</button>
+    </form>
       </div>
       <br/>
       <div className="survey-footer">
