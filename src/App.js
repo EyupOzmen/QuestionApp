446d5e278760e939965ff3fd11dfc598aebae20b
EyupOzmen <ozmeneyup60@gmail.com>
2020-10-20 22:17:58 +0300

    "Multisingle"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Col, Container,Row } from 'react-bootstrap';
-import Survey from './components/Survey'
+import MultiSingle from './components/MultiSingle'
+import question from "./data/question.json";
 
 const App = () => {
     return(
         <Container>
             <Row>
-                <Col><Survey/></Col>
+                <Col>
+                <MultiSingle
+                    header={question.HeaderLogo}
+                    survey={question.Questions[0].Question}
+                    optionsArr={question.Questions[0].Options}
+                    footer={question.FooterLogo}
+                />
+                </Col>
             </Row>    
         </Container>
     )
